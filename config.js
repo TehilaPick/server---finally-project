fa33ec9702e4db03afba32716473698bd46e1bc6
Esther Winkler <5710490@gmail.comgit config --global user.name>
2022-01-23 00:45:35 +0200

    עיצחת

--- config.js
+++ config.js
@@ -1,6 +1,8 @@
 
 
 const config= {
+
+    //תהילה
     db: {
         host: '127.0.0.1',
         user: 'root',
@@ -8,6 +10,15 @@ const config= {
         database: 'finallyproject',
         port: '3306',
     }
+
+    //אסתי
+    // db: {
+    //     host: '127.0.0.1',
+    //     user: 'root',
+    //     password: 'Bu2021!esti',
+    //     database: 'new_schema',
+    //     port: '3306',
+    // }
 }
 
 module.exports = config
